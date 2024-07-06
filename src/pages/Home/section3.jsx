414992cb6594fd4878d18108849906956f4f2aaf
Ken Udoudo <kendo302003@gmail.com>
2024-07-06 10:46:46 -0700

    fuck

--- src/pages/Home/section3.jsx
+++ src/pages/Home/section3.jsx
@@ -1,7 +1,15 @@
 const Sec3home = () => {
     return (  
         <div className="px-[20px] md:px-[40px] py-[20px]">
-            <p className="text-[38px]">Featured Products</p>
+            <p className="text-[38px] font-semibold">Featured Products</p>
+
+            <div className="w-full flex flex-col flex-wrap gap-[10px]">
+                <div className="w-[45%] md:w-[33%] lg:w-[25%]">
+                    <div className="w-full border rounded-sm">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
