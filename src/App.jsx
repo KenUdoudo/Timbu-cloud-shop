86a79d07bd31f9b82cf153eeb9246e78a422e8cd
Ken Udoudo <kendo302003@gmail.com>
2024-07-06 07:55:34 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import Products from './pages/products';
+import Contact from './pages/contactus';
+import Home from './pages/Home';
+import MyCart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
@@ -11,8 +16,9 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/products' element={<Products />} />
-            <Route path='/contactus' element={<Events />} />
-            <Route path='/mycart' element={<About />} />
+            <Route path='/contactus' element={<Contact />} />
+            <Route path='/mycart' element={<MyCart />} />
+            <Route path='/checkout' element={<Checkout />} />
           </Routes>
         </div>
         <Footer />
