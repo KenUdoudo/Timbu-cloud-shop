b236783dc9523da1ea318fcb3b22d47b7ae8d972
Ken Udoudo <kendo302003@gmail.com>
2024-07-07 13:25:23 -0700

    almost

--- src/pages/Home/section2.jsx
+++ src/pages/Home/section2.jsx
@@ -1,12 +1,13 @@
-import { RiArrowDropDownLine } from "react-icons/ri";
+
+import { PiCheckFat } from "react-icons/pi";
 const Sec2home = () => {
     return (  
         <div className="px-[20px] md:px-[40px] py-[20px]">
-            <div className="flex flex-row justify-between items-center">
-                <p className="flex items-center text-nowrap text-[8px] md:text-[16px]">Payment Method <RiArrowDropDownLine className="w-[15px] md:w-[30px] h-[15px] md:h-[30px]"/></p>
-                <p className="flex items-center text-nowrap text-[8px] md:text-[16px]">Quick Customer Service <RiArrowDropDownLine className="w-[15px] md:w-[30px] h-[15px] md:h-[30px]"/></p>
-                <p className="flex items-center text-nowrap text-[8px] md:text-[16px]">Product Quality <RiArrowDropDownLine className="w-[15px] md:w-[30px] h-[15px] md:h-[30px]"/></p>
-                <p className="flex items-center text-nowrap text-[8px] md:text-[16px]">Fast Delivery <RiArrowDropDownLine className="w-[15px] md:w-[30px] h-[15px] md:h-[30px]"/></p>
+            <div className="flex flex-row justify-between items-center flex-wrap">
+                <p className="flex items-center gap-[10px] text-nowrap text-[10px] md:text-[16px]">Payment Method <PiCheckFat className="w-[15px] md:w-[20px] h-[15px] md:h-[20px]"/></p>
+                <p className="flex items-center gap-[10px] text-nowrap text-[10px] md:text-[16px]">Quick Customer Service <PiCheckFat className="w-[15px] md:w-[20px] h-[15px] md:h-[20px]"/></p>
+                <p className="flex items-center gap-[10px] text-nowrap text-[10px] md:text-[16px]">Product Quality <PiCheckFat className="w-[15px] md:w-[20px] h-[15px] md:h-[20px]"/></p>
+                <p className="flex items-center gap-[10px] text-nowrap text-[10px] md:text-[16px]">Fast Delivery <PiCheckFat className="w-[15px] md:w-[20px] h-[15px] md:h-[20px]"/></p>
             </div>
             <hr className="my-[15px]"/>
         </div>
