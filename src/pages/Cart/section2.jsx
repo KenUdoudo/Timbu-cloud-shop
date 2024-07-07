7021c5a1010bf32bbec1f519291d803351d9a6ea
Ken Udoudo <kendo302003@gmail.com>
2024-07-07 14:33:17 -0700

    cart

---
+++ src/pages/Cart/section2.jsx
@@ -0,0 +1,14 @@
+const Sec2cart = () => {
+    return (
+        <div className="px-[20px] md:px-[40px] w-full">
+            <div className="w-[80%]">
+
+
+
+            </div>
+
+        </div>
+    );
+}
+ 
+export default Sec2cart;
