d49e255483ccea810e0e8c585488b0d008633e1a
Ken Udoudo <kendo302003@gmail.com>
2024-07-08 12:05:53 -0700

    omo

--- src/pages/Cart/section2copy.jsx
+++ src/pages/Cart/section2copy.jsx
@@ -7,7 +7,7 @@ const Sec2cart = () => {
     
 
     return (
-        <div className="px-[20px] md:px-[40px] w-full mt-[70px]">
+        <div className="px-[20px] md:px-[40px] w-full mt-[70px] flex flex-row items-start gap-x-[5px]">
             <div className="w-[75%]">
               <div className="w-full flex justify-around font-semibold">
                 <p className="px-[90px]">PRODUCT NAME</p>
@@ -36,7 +36,29 @@ const Sec2cart = () => {
 
 
             </div>
-            
+            <div className="w-[25%] border-[1px] border-[#9DBEFF] rounded-[8px] p-[10px]">
+              <p className="font-semibold">Order Summary</p>
+              <div className="w-full flex justify-between mt-[20px]">
+                <p className="text-[#818181] text-[13px]">Sub-Total</p>
+                <p className="text-[#2972FF] text-[13px]">₦12,150,560</p>
+              </div>
+              <div className="w-full flex justify-between mt-[8px]">
+                <p className="text-[#818181] text-[13px]">Shipping Fee</p>
+                <p className="text-[#2972FF] text-[13px]">₦83,300</p>
+              </div>
+              <hr className="w-full my-[10px]"/>
+
+              <div className="w-full flex items-center">
+                <input type="text" placeholder="Coupon Code" className="py-[5px] pl-[5px] pr-[10px] text-[10px] border-[1px] rounded-[8px]"/>
+                <p className="bg-[#70A1FF] text-white px-[20px] py-[5px] rounded-[8px] text-[13px]">Apply</p>
+              </div>
+
+              <div className="w-full flex justify-between mt-[8px]">
+                <p className="text-[14px]">Total Price</p>
+                <p className="text-[#2972FF] text-[13px]">₦12,223,860</p>
+              </div>
+              <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white w-full py-[7px] rounded-[8px] cursor-pointer mt-[20px] text-center">Checkout</p>
+            </div>
 
         </div>
     );
--- src/pages/Home/section3.jsx
+++ src/pages/Home/section3.jsx
@@ -19,7 +19,7 @@ const Sec3home = () => {
 
             <div className="w-full flex flex-row justify-center flex-wrap gap-[20px] mt-[20px] md:mt-[40px]">
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ ac } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>BRUHM BLACK AC BAS-09ICQB</p>
@@ -30,7 +30,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ tv } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>BRUHM LED BTF43SV FHD , SMART OS TV</p>
@@ -41,7 +41,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ blender } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>BINATONE BLENDER</p>
@@ -52,7 +52,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ fridge } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>BRUHM BLACK REF BFQ-592EN </p>
@@ -63,7 +63,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ icemaker } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>HISENSE 20KG ICE MAKER ICM2011</p>
@@ -74,7 +74,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ airfryer } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>HISENSE H04AfBK1S1 4.5L AIR FRYER</p>
@@ -85,7 +85,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ washing } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>LG F2V5FGPYJE 9/5KG FRONT LOAD</p>
@@ -96,7 +96,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ four } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>HISENSE 100 INCH U7K SERIES ULED 4K</p>
@@ -107,7 +107,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ power } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>HUAWEI -M 2.5kW POWER  INVERTER</p>
@@ -118,7 +118,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ gen } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>MAXI E17000KWH GENERATOR 21.25 KVA</p>
@@ -129,7 +129,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ toaster } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>MAXI TOASTER 2 SLICES</p>
@@ -140,7 +140,7 @@ const Sec3home = () => {
                     <p className="bg-[#2972FF] hover:bg-[#6893eb] text-white px-[20px] py-[5px] rounded-[8px] flex justify-center items-center gap-[10px] cursor-pointer mt-[15px] text-[16px]"><TbShoppingBag /> Add to Cart</p>
                 </div>
                 <div className="w-full md:w-[30%] lg:w-[20%]">
-                    <div className="w-full border-[2px] rounded-[8px] flex justify-center items-center  h-[300px]">
+                    <div className="w-full border-[2px] border-[#9DBEFF] rounded-[8px] flex justify-center items-center  h-[300px]">
                         <img src={ solar } className="w-auto h-[250px]"/>
                     </div>
                     <p className='text-[9px] mt-[10px]'>MAXI TOASTER 2 SLICES</p>
