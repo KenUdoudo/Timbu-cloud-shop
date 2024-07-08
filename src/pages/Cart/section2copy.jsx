866f2c912e45a79a34c909913f93e553ae48292e
Ken Udoudo <kendo302003@gmail.com>
2024-07-08 11:31:42 -0700

    almost

--- src/pages/Cart/section2copy.jsx
+++ src/pages/Cart/section2copy.jsx
@@ -1,26 +1,35 @@
 import { HiOutlineTrash } from "react-icons/hi2";
+import { FaPlusSquare } from "react-icons/fa";
+import { FaMinusSquare } from "react-icons/fa";
 import four from './assets/4k.png';
 
 const Sec2cart = () => {
     
 
     return (
-        <div className="px-[20px] md:px-[40px] w-full">
+        <div className="px-[20px] md:px-[40px] w-full mt-[70px]">
             <div className="w-[75%]">
-              <div className="w-full flex justify-around">
-                <p>PRODUCT NAME</p>
+              <div className="w-full flex justify-around font-semibold">
+                <p className="px-[90px]">PRODUCT NAME</p>
                 <p>PRICE</p>
                 <p>QUANTITY</p>
                 <p>TOTAL</p>
               </div>
               <div className="w-full">
-                <hr className="my-[20px] "/>
-                <div className="w-full flex justify-between">
-                  <div className="flex items-center">
+                <hr className="mt-[20px] "/>
+                <div className="w-full flex justify-between items-center">
+                  <div className="flex items-center gap-[5px]">
                     <HiOutlineTrash className="text-[#DD2C00]"/>
                     <img src={ four } className="w-[100px]"/>
                     <p className="text-[10px]">HISENSE 100 INCH U7K SERIES ULED 4K</p>
                   </div>
+                  <p className="font-semibold">₦3,900,990</p>
+                  <div className="flex items-center gap-[5px]">
+                    <FaMinusSquare />
+                    <p className="text-[12px]">1</p>
+                    <FaPlusSquare className="text-[#2972FF]"/>
+                  </div>
+                  <p className="font-semibold">₦3,900,990</p>
                 </div>
               </div>
 
