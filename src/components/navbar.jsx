f69c4c961e9e9a15f35cb617d0646b3af6181485
Ken Udoudo <kendo302003@gmail.com>
2024-07-09 02:23:20 -0700

    omo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ import { TbShoppingBag } from "react-icons/tb";
 import Search from "./search";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { CiSearch } from "react-icons/ci";
+import { PiCaretDownLight } from "react-icons/pi";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -32,30 +33,23 @@ const Navbar = () => {
           <p className="bg-[#2972FF] border-[2px] border-white w-[20px] h-[20px] flex justify-center items-center rounded-full text-[10px] top-[-7px] right-[-7px] absolute text-white">5</p>
           </div>
         <div
-          className={`fixed top-0 left-0 h-full z-20 bg-[#e9e9e9] w-[75%] md:w-[50%] transform ${
+          className={`fixed top-0 left-0 h-full z-20 bg-white w-[75%] md:w-[50%] transform ${
             isOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-500 ease-in-out overflow-hidden`}
         >
-          <div className="flex flex-col justify-between items-center py-[40px] gap-[25px]">
-            <Link to="/products">Products</Link>
-            <Link to="/contactus">Contact Us</Link>
-            <div className="flex flex-row items-center w-[250px] text-[8px] h-[42px] border bg-white border-[#2972FF] rounded-[12px]">
+          <div className="flex flex-col justify-between items-start pt-[60px] pl-[20px] gap-[25px]">
+          <div className="flex flex-row items-center w-[250px] text-[12px] h-[42px] border bg-white border-[#818181] rounded-[12px]">
               <CiSearch className="flex-shrink-0 ml-[10px]" />
               <input
                 type="text"
                 className="w-full py-2 ml-[10px] focus:outline-none"
                 placeholder="Search for all accessories"
               />
-              <select className="px-[10px] focus:outline-none hidden lg:block">
-                <option>
-                  All Categories
-                  <RiArrowDropDownLine />
-                </option>
-              </select>
-              <button className="h-full px-[20px] bg-[#2972FF] text-white rounded-r-[12px]">
-                Search
-              </button>
             </div>
+            <p className="flex gap-[10px] items-center text-[12px]">All Categories <PiCaretDownLight className="w-[20px] h-[20px]"/></p>
+            <Link to="/notfound" className="text-[12px]">Products</Link>
+            <Link to="/notfound" className="text-[12px]">Contact Us</Link>
+            
           </div>
         </div>
 
