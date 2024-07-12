f3d7860012ed431c92b1ec8fabbbb0fc03f871ee
Ken Udoudo <kendo302003@gmail.com>
2024-07-12 07:34:29 -0700

    huh

---
+++ src/pages/Cart/indexcopy.jsx
@@ -0,0 +1,14 @@
+import Sec1cart from "./section1";
+import Sec2cart from "./section2";
+
+const MyCart = () => {
+    return (
+        <div className="font-pop">
+            <Sec1cart />
+
+            <Sec2cart />
+        </div>
+    );
+}
+ 
+export default MyCart;
